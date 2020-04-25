c8841ef856f89a582d9849b5fb746351e6d9dc71
Mandy Wang <shunflower320@gmail.com>
2020-04-25 20:03:36 +0800

    rocchio mostly done, missing last step

--- Homework #4/queries/query_refinement.py
+++ Homework #4/queries/query_refinement.py
@@ -6,8 +6,8 @@ Finds the normalised tf.idf values for all tokens, and stores it in a dictionary
 
 :param tokens: tokens to find
 """
-def get_normalised_tf_idf(tokens, dictionary, postings, doc_lengths):
-    scores = {}  # key: docID, value: cosine score
+def get_total_normalised_tf_idf(tokens, dictionary, postings, doc_lengths):
+    scores_for_combined_vector = {}  # key: docID, value: cosine score
     for token in tokens:
         for docID in postings[token]:
             # calculate weighted token frequency
@@ -15,32 +15,34 @@ def get_normalised_tf_idf(tokens, dictionary, postings, doc_lengths):
             ltf = 1 + math.log(tf, 10)
 
             idf = math.log(1/dictionary[token][0], 10) # lg(1/df)
-            # update scores
-            if token in scores:
-                scores[token] += ltf * idf
+            doc_length = doc_lengths[docID]
+
+            # update scores, normalising all scores by dividing by document length for each doc
+            if token in scores_for_combined_vector:
+                scores_for_combined_vector[token] += ltf * idf / doc_length
             else:
-                scores[token] = ltf * idf
+                scores_for_combined_vector[token] = ltf * idf  / doc_length
 
-    # normalise all scores by dividing by document length
-    for token, score in scores.items():
-        doc_length = doc_lengths[docID]
-        scores[token] = score / doc_length
-
-    return scores
+    return scores_for_combined_vector
 
 """
 Rocchio, assuming documents have been indexed
+
+:param query: { query_token: original_tf.idf } 
 :param top_k: list of top relevant docIDs
 :param documents: indexed documents { docID: content }
 """
-def rocchio(top_k, dictionary, postings_file, doc_lengths, documents):
+def rocchio(query, top_k, dictionary, postings_file, doc_lengths, documents):
+
+    alpha = 0.2 # magic number 1
+    beta = 0.3 # magic number 2
+
     # index document with { docID: { token: tf.idf } }
     # find centroid with top_k doc
     # 1. sum the tf.idf of each token of all relevant docs
-    #   1.1 for tokens that do not exist in a doc, add 0
     # 2. divide each sum by no. of docs
     # 3. multiply by weight and add to original query
-    # 4. Take the new dictionary of { token: value} and run cosine similarity on all docs agn.
+    # 4. Take the new dictionary of { token: value } and run cosine similarity on all docs agn.
 
     tokens = set() # unique terms
 
@@ -55,9 +57,20 @@ def rocchio(top_k, dictionary, postings_file, doc_lengths, documents):
 
     postings = get_postings(list(tokens), dictionary, postings_file)
 
-    for token in tokens:
-        token_with_tf_idf = get_normalised_tf_idf(tokens, dictionary, postings_file, doc_lengths) # { token: tf.idf }
+    tokens_dict_with_tf_idf = get_total_normalised_tf_idf(tokens, dictionary, postings, doc_lengths) # { token: total_normalised_tf.idf }
 
+    # divide by no. of docs
+    for token in tokens_dict_with_tf_idf:
+        tokens_dict_with_tf_idf[token] /= len(top_k)
+
+    new_query_vector = {}
+    for token in tokens_dict_with_tf_idf:
+        if token not in query:
+            new_query_vector[token] = beta * tokens_dict_with_tf_idf[token]
+        else:
+            new_query_vector[token] = alpha * query[token] + beta * tokens_dict_with_tf_idf[token]
+
+    find_cosine_similarity_against_docs_agn
 
 """
 Assumes top-k searches are relevant, and evaluates relevance feedback accordingly.
